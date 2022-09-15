4498faa78a0bef81d5b0b119e3eea7f513edc311
@eko.prasetiyo <eko.prasetiyo@sent.tv>
2022-09-15 08:16:16 +0700

    init project

---
+++ ios/RCTNativeModules.h
@@ -0,0 +1,15 @@
+//
+//  RCTNativeModules.h
+//  alami
+//
+//  Created by Eko Prasetiyo on 15/09/22.
+//
+#import <React/RCTBridgeModule.h>
+
+#ifndef RCTNativeModules_h
+#define RCTNativeModules_h
+
+@interface RCTNativeModules : NSObject <RCTBridgeModule>
+@end
+
+#endif /* RCTNativeModules_h */
